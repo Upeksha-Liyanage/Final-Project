a18cddafb61ec6520559ff84f28de65753534345
pasan <pasanheshan@gmail.com>
2019-08-31 12:29:45 +0530

    first push

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -15,7 +15,29 @@ def compile(request):
 
 # def folder(path):
 #     # subprocess.check_call(['xdg-open', '--', path])
-# 	# file = open("/home")
-# 	file="/"
+# 	file = open("/home")
+# 	# file="/"
 #     path=os.getcwd()+file
 #     fp=open(path,'r+')
+
+
+from tkinter import filedialog
+from tkinter import *
+
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    print(filename)
+
+
+root = Tk()
+folder_path = StringVar()
+lbl1 = Label(master=root,textvariable=folder_path)
+lbl1.grid(row=0, column=1)
+button2 = Button(text="Browse", command=browse_button)
+button2.grid(row=0, column=3)
+
+mainloop()
